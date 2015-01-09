ff53597306209904415d64a819bdf8e95a9128f7
Mingding <hanmd82@gmail.com>
2015-01-09 14:20:18 +0800

    Modify site layout using Bootstrap CSS

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby '2.2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
+gem 'bootstrap-sass', '3.2.0.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-
-  get 'static_pages/help'
+  root          'static_pages#home'
+  get 'help' => 'static_pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
