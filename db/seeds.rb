0420d5ce03ddb1f4ac34b27601cc13ae531f9626
Mingding <hanmd82@gmail.com>
2015-01-16 11:08:36 +0800

    Seed database with sample data for API testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Node.create!(guid: 1101, label: "Node 1101 (Singapore Zoo)", lat: 1.404321, lng: 103.792937, description: "This is the node with GUID 1101 at the Singapore Zoo")
+Node.create!(guid: 1102, label: "Node 1102 (Singapore Night Safari)", lat: 1.404322, lng: 103.792938, description: "This is the node with GUID 1102 at the Singapore Night Safari")
